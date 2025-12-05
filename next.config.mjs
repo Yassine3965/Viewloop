95882d13e44848ef99ea212e29acce056e594c83
Yassine Nahim <AYNsad3965@gmail.com>
2025-12-05 12:23:28 +0000

    content_bridge.js:1 🔐 ViewLoop Auth Bridge Injected! content_bridge.js:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ytimg.com',
+                port: '',
+                pathname: '/vi/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'source.boringavatars.com',
+                port: '',
+                pathname: '/**',
+            }
+        ],
+    },
+};
 
 export default nextConfig;
