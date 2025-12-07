bf85f361ce8e1769d6b0457857fb4a5fe67d0072
Yassine Nahim <AYNsad3965@gmail.com>
2025-12-07 00:50:35 +0000

    2. الوقت لا يعد في content_youtube.js text ⏱️  الوقت: 0 ثانية → 0.00 نقط

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    },
     images: {
         remotePatterns: [
             {
@@ -11,17 +25,9 @@ const nextConfig = {
             {
                 protocol: 'https',
                 hostname: 'images.unsplash.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'source.boringavatars.com',
-                port: '',
-                pathname: '/**',
             }
-        ],
-    },
+        ]
+    }
 };
 
 export default nextConfig;
