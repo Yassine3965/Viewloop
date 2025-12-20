9b8e6296d9608f4a392701606341a1fa8d3ce4bd
unknown <aynsad3965@gmail.com>
2025-12-20 22:26:45 +0100

    Fix email

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,8 @@ export default function RootLayout({
         />
         <link
           rel="icon"
-          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='hsl(205, 80%, 50%)' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'></circle><polygon points='10 8 16 12 10 16 10 8'></polygon></svg>"
+          href="/logo.png"
+          type="image/png"
         />
         <script
           dangerouslySetInnerHTML={{
